e4d474ab0b025fb1ed8dcef05d3e31680928d5f1
LeanderRoelandtDev <leanderroelandt1@hotmail.be>
2026-08-31 20:58:14 +0200

    Added Project base structure + dependencies

---
+++ UrlShortener.Api/Controllers/UrlController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UrlShortener.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UrlController : ControllerBase
+    {
+        
+    }
+}
