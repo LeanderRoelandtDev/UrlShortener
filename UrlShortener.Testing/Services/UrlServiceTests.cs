d26f512e2e217297194789bcd73ad5ebb8da135d
LeanderRoelandtDev <leanderroelandt1@hotmail.be>
2026-09-16 19:03:06 +0200

    Added first test using Xunit and moq library Added AssemblyInfo class to use the UrlServices outside it intended scope (here for .Testing) Ran first successfull

---
+++ UrlShortener.Testing/Services/UrlServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using UrlShortener.Core.Interfaces.Repositories;
+using UrlShortener.Core.Interfaces.Services;
+using UrlShortener.Core.Services;
+
+namespace UrlShortener.Testing.Services
+{
+    public class UrlServiceTests
+    {
+        [Fact]
+        public async Task GetOriginalUrl_ReturnsUrlWhenAvaiable()
+        {
+            //Creates mock of the UrlRepository
+            Moq.Mock<UrlShortener.Core.Interfaces.Repositories.IUrlRepository> repository = new Mock<IUrlRepository>();
+
+            repository.Setup(x => x.GetOriginalUrl("qwertyui"))
+                      .ReturnsAsync("https://google.com");
+
+            UrlService service = new UrlService(repository.Object);
+
+            //Call the method inside the UrlService
+            string result = await service.GetOriginalUrl("qwertyui");
+
+            //Check if the returning url matches what i expect
+            Assert.Equal("https://google.com", result);
+        }
+    }
+}
