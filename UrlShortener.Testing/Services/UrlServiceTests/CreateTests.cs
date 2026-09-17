72651c32c4e82ee918c24e2d57553233272ba48e
LeanderRoelandtDev <leanderroelandt1@hotmail.be>
2026-09-17 16:21:23 +0200

    Added unit tests for urlservice

---
+++ UrlShortener.Testing/Services/UrlServiceTests/CreateTests.cs
@@ -0,0 +1,132 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using UrlShortener.Core.Exceptions;
+using UrlShortener.Core.Interfaces.Repositories;
+using UrlShortener.Core.Interfaces.Services;
+using UrlShortener.Core.Services;
+using UrlShortener.Dtos.Url.Request;
+using static System.Net.WebRequestMethods;
+
+namespace UrlShortener.Testing.Services.UrlServiceTests
+{
+    public class CreateTests()
+    {
+        [Fact]
+        public async Task Create_WhenCodeIsUnique()
+        {
+            //Creates mock of the UrlRepository
+            Moq.Mock<IUrlRepository> repository = new Mock<IUrlRepository>();
+
+            repository.Setup(x => x.IsDuplicate(It.IsAny<string>()))
+                                   .ReturnsAsync(false);
+
+            repository.Setup(x => x.Create("https://google.com", It.IsAny<string>()))
+                      .ReturnsAsync((string url, string shortUrl) => shortUrl);
+
+
+            UrlService service = new UrlService(repository.Object);
+
+            //Create the dto
+            CreateShortUrlRequest request = new CreateShortUrlRequest
+            {
+                Url = "https://google.com"
+            };
+
+
+            //Call the method inside the UrlService and store the return
+            string result = await service.Create(request);
+
+
+            //Check if the returning url matches what i expect
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Create_WhenCodeIsADuplicateOnce()
+        {
+            //Creates mock of the UrlRepository
+            Moq.Mock<IUrlRepository> repository = new Mock<IUrlRepository>();
+
+            repository.SetupSequence(x => x.IsDuplicate(It.IsAny<string>()))
+                                   .ReturnsAsync(true)
+                                   .ReturnsAsync(false);
+
+            repository.Setup(x => x.Create("https://google.com", It.IsAny<string>()))
+                      .ReturnsAsync((string url, string shortUrl) => shortUrl);
+
+
+            UrlService service = new UrlService(repository.Object);
+
+            //Create the dto
+            CreateShortUrlRequest request = new CreateShortUrlRequest
+            {
+                Url = "https://google.com"
+            };
+
+
+            //Call the method inside the UrlService and store the return
+            string result = await service.Create(request);
+
+            //Check if the returning url matches what i expect
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Create_WhenCodeIsADuplicateTwice()
+        {
+            //Creates mock of the UrlRepository
+            Moq.Mock<IUrlRepository> repository = new Mock<IUrlRepository>();
+
+            repository.SetupSequence(x => x.IsDuplicate(It.IsAny<string>()))
+                                   .ReturnsAsync(true)
+                                   .ReturnsAsync(true);
+
+
+            UrlService service = new UrlService(repository.Object);
+
+            //Create the dto
+            CreateShortUrlRequest request = new CreateShortUrlRequest
+            {
+                Url = "https://google.com"
+            };
+
+
+            //Call the method inside the UrlService and store the return
+            ErrorException exception = await Assert.ThrowsAsync<ErrorException>(() => service.Create(request));
+
+            Assert.Equal(HttpStatusCode.Conflict, exception.Code);
+        }
+
+        [Fact]
+        public async Task Create_RepoReturnsError()
+        {
+            //Creates mock of the UrlRepository
+            Moq.Mock<IUrlRepository> repository = new Mock<IUrlRepository>();
+
+            repository.Setup(x => x.IsDuplicate(It.IsAny<string>()))
+                                   .ReturnsAsync(false);
+
+            repository.Setup(x => x.Create("https://google.com", It.IsAny<string>()))
+                      .ThrowsAsync(new Exception("Something went wrong creating a UrlEntity"));
+
+
+            UrlService service = new UrlService(repository.Object);
+
+            //Create the dto
+            CreateShortUrlRequest request = new CreateShortUrlRequest
+            {
+                Url = "https://google.com"
+            };
+
+
+            //Call the method inside the UrlService and store the return
+            Exception exception = await Assert.ThrowsAsync<Exception>(() => service.Create(request));
+
+            //Check if the returning url matches what i expect
+            Assert.Equal("Something went wrong creating a UrlEntity", exception.Message);
+        }
+    }
+}
---
+++ UrlShortener.Testing/Services/UrlServiceTests/GetOriginalUrlTests.cs
@@ -0,0 +1,47 @@
+﻿using Moq;
+using System.Net;
+using UrlShortener.Core.Exceptions;
+using UrlShortener.Core.Interfaces.Repositories;
+using UrlShortener.Core.Services;
+
+namespace UrlShortener.Testing.Services.UrlServiceTests
+{
+    public class GetOriginalUrlTests
+    {
+        [Fact]
+        public async Task GetOriginalUrl_ReturnsUrlWhenAvaiable()
+        {
+            //Creates mock of the UrlRepository
+            Moq.Mock<IUrlRepository> repository = new Mock<IUrlRepository>();
+
+            repository.Setup(x => x.GetOriginalUrl("qwertyui"))
+                      .ReturnsAsync("https://google.com");
+
+            UrlService service = new UrlService(repository.Object);
+
+            //Call the method inside the UrlService
+            string result = await service.GetOriginalUrl("qwertyui");
+
+            //Check if the returning url matches what i expect
+            Assert.Equal("https://google.com", result);
+        }
+
+        [Fact]
+        public async Task GetOriginalUrl_ThrowsNotFound_WhenUrlDoesNotExist()
+        {
+            //Creates mock of the UrlRepository
+            Moq.Mock<IUrlRepository> repository = new Mock<IUrlRepository>();
+
+            repository.Setup(x => x.GetOriginalUrl("qwertyui"))
+                      .ReturnsAsync((string?)null);
+
+            UrlService urlService = new UrlService(repository.Object);
+
+            //Call the method inside the UrlService
+            ErrorException exception = await Assert.ThrowsAsync<ErrorException>(
+                () => urlService.GetOriginalUrl("qwertyui"));
+
+            Assert.Equal(HttpStatusCode.NotFound, exception.Code);
+        }
+    }
+}
